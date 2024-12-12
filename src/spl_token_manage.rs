af074cb7f6bb44717f02a2c3c0821fb9a62c1ce9
Davirain <davirain.yin@gmail.com>
2024-12-12 10:09:52 +0800

    Revert "feat: add SPL token transfer functionality"

--- src/spl_token_manage.rs
+++ src/spl_token_manage.rs
@@ -2,7 +2,6 @@ use clap::Parser;
 
 pub mod create_token;
 pub mod get_balance;
-pub mod transfer_token;
 
 #[derive(Parser, Debug)]
 pub enum SplTokenMange {
@@ -10,8 +9,6 @@ pub enum SplTokenMange {
     CreateToken(create_token::CreateTokenArgs),
     /// get spl token balance
     GetBalance(get_balance::GetBalanceArgs),
-    /// transfer spl token
-    TransferToken(transfer_token::TransferTokenArgs),
 }
 
 pub async fn handle_spl_token_manage(spl_token_manage: &SplTokenMange) -> anyhow::Result<()> {
@@ -20,6 +17,5 @@ pub async fn handle_spl_token_manage(spl_token_manage: &SplTokenMange) -> anyhow
             create_token::handle_create_token(create_token_args).await
         }
         SplTokenMange::GetBalance(args) => get_balance::handle_get_balance(args).await,
-        SplTokenMange::TransferToken(args) => transfer_token::handle_transfer_token(args).await,
     }
 }
